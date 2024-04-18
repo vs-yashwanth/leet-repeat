31be06ed1ec57f565eb03cbec68d0bd9782a727a
yash <yashwanthvs@gmail.com>
2024-04-19 00:04:22 +0530

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import leetLogo from "/leetcode.svg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://leetcode.com/problems" target="_blank">
+          <img src={leetLogo} height={50} width={50} alt="Leetcode logo" />
+        </a>
+      </div>
+      <h1>Vite + Leet + React</h1>
+    </>
+  );
+}
+
+export default App;
