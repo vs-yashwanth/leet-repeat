74d42cbb9760eeecbda4457975be827637a68d87
yash <yashwanthvs@gmail.com>
2024-11-06 21:15:46 +0530

    Restructure folders

---
+++ client/src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ4-kh2DNqDUaGbe1VQLO1fmKxOv7GoJU",
+  authDomain: "leet-repeat.firebaseapp.com",
+  projectId: "leet-repeat",
+  storageBucket: "leet-repeat.appspot.com",
+  messagingSenderId: "621462055637",
+  appId: "1:621462055637:web:06e6ba0ba56c61a18d905c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
